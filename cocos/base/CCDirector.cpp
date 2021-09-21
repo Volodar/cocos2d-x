2359187d0984d7aa82f95a848265c42b7d036b06
Vladimir Tolmachev <tolm_vl@hotmail.com>
2021-09-21 15:48:43 +0700

    changed position of stats

--- cocos/base/CCDirector.cpp
+++ cocos/base/CCDirector.cpp
@@ -75,7 +75,7 @@ THE SOFTWARE.
  Default: 0,0 (bottom-left corner)
  */
 #ifndef CC_DIRECTOR_STATS_POSITION
-#define CC_DIRECTOR_STATS_POSITION Director::getInstance()->getVisibleOrigin()
+#define CC_DIRECTOR_STATS_POSITION (Director::getInstance()->getVisibleOrigin() + cocos2d::Point(0, 100))
 #endif // CC_DIRECTOR_STATS_POSITION
 
 using namespace std;

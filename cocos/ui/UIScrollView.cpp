43d8b1b667ef45fd112847e4d15a70aabc8e0b9a
Vladimir Tolmachev <tolm_vl@hotmail.com>
2025-03-29 09:35:08 +0700

    fix NaN operation

--- cocos/ui/UIScrollView.cpp
+++ cocos/ui/UIScrollView.cpp
@@ -811,11 +811,11 @@ void ScrollView::scrollToPercentBothDirection(const Vec2& percent, float timeInS
     
 float ScrollView::getScrolledPercentVertical() const {
     const float minY = getContentSize().height - getInnerContainerSize().height;
-    return (1.f - getInnerContainerPosition().y / minY)*100.f;
+    return minY != 0 ? (1.f - getInnerContainerPosition().y / minY)*100.f : 0;
 }
 float ScrollView::getScrolledPercentHorizontal() const {
     const float minX = getContentSize().width - getInnerContainerSize().width;
-    return getInnerContainerPosition().x / minX * 100.f;
+    return minX != 0 ? getInnerContainerPosition().x / minX * 100.f : 0;
 }
 Vec2 ScrollView::getScrolledPercentBothDirection() const {
     return {getScrolledPercentHorizontal(), getScrolledPercentVertical()};
